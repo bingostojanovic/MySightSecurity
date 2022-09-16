0286236052ec5999aa5d7cd28f4944c3bdea2517
bingo <tonybingo970423@outlook.com>
2022-09-16 14:32:08 +0200

    UI nav title completed successfully.

--- app/src/main/java/com/justin/mysightsecurity/MainActivity.java
+++ app/src/main/java/com/justin/mysightsecurity/MainActivity.java
@@ -3,12 +3,16 @@ package com.justin.mysightsecurity;
 import android.os.Bundle;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -22,6 +26,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        titleText = findViewById(R.id.titleText);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                
+                switch(navDestination.getId()){
+
+                    case R.id.pin_input:
+                        titleText.setText("Pin Input");
+                        break;
+                    case R.id.history:
+                        titleText.setText("History");
+                        break;
+                    case R.id.add_device:
+                        titleText.setText("Add New Device");
+                        break;
+                    case R.id.pin_setup:
+                        titleText.setText("Setup");
+                        break;
+                    case R.id.scan:
+                        titleText.setText("Gallery");
+                        break;
+                    default:
+                        titleText.setText("");
+                        break;
+
+                }
+            }
+        });
     }
 }
