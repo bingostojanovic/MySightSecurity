0ae9a65834ef76bbec5b7a7b3a843545ad4db193
bingo <tonybingo970423@outlook.com>
2022-09-16 22:56:10 +0200

    Pin Input database completed!

--- app/src/main/java/com/justin/mysightsecurity/ui/pin_input/PinInputFragment.java
+++ app/src/main/java/com/justin/mysightsecurity/ui/pin_input/PinInputFragment.java
@@ -1,11 +1,14 @@
 package com.justin.mysightsecurity.ui.pin_input;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.justin.mysightsecurity.MainActivity;
 import com.justin.mysightsecurity.PinInputActivity;
+import com.justin.mysightsecurity.PinSetupActivity;
 import com.justin.mysightsecurity.SplashActivity;
 import com.justin.mysightsecurity.databinding.FragmentPininputBinding;
 
@@ -31,8 +35,9 @@ public class PinInputFragment extends Fragment {
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        Intent pininput = new Intent(getActivity(), PinInputActivity.class);
-        ((MainActivity)getActivity()).startActivity(pininput);
+//        Intent pininput = new Intent(getActivity(), PinInputActivity.class);
+//        ((MainActivity)getActivity()).startActivity(pininput);
+
 
         return root;
     }
